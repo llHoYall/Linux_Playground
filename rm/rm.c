2b77b85431d0cde3f0cdaf9ce7ce0b647e23d584
llHoYall <hoya128@gmail.com>
2019-02-06 13:21:46 +0000

    Add fake rm program

---
+++ rm/rm.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ *	@brief		fake rm program.
+ *	@author		llHoYall <hoya128@gmail.com>
+ *	@version	v1.0.0
+ *	@history
+ *		2019.02.06	Created.
+ ******************************************************************************/
+
+/* Include Headers -----------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Main Routines -------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "%s: no arguments\n", argv[0]);
+		exit(1);
+	}
+
+	for (int i = 1; i < argc; ++i) {
+		if (unlink(argv[i]) < 0) {
+			perror(argv[i]);
+			exit(1);
+		}
+	}
+
+	exit(0);
+}
+
---
+++ rm/rm.md
@@ -0,0 +1,12 @@
+# rm
+
+fake `rm` program.
+
+## Run
+
+> $ mkdir build && cd build && cmake .. && make
+
+## Usage
+
+* `$ ./rm <files>`
+
